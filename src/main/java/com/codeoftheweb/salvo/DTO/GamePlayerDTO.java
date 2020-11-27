2a32234ef3070a499e3c44bf905f8bf819345104
franpassa <francopassarelli94@gmail.com>
2020-11-27 16:25:44 -0300

    Primer commit

---
+++ src/main/java/com/codeoftheweb/salvo/DTO/GamePlayerDTO.java
@@ -0,0 +1,65 @@
+package com.codeoftheweb.salvo.DTO;
+
+import com.codeoftheweb.salvo.model.GamePlayer;
+import com.codeoftheweb.salvo.model.Salvo;
+import com.codeoftheweb.salvo.model.Ship;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class GamePlayerDTO {
+
+    GamePlayer gamePlayer;
+
+    public GamePlayer getGamePlayer() { return gamePlayer; }
+
+    public void setGamePlayer(GamePlayer gamePlayer) { this.gamePlayer = gamePlayer; }
+
+    public GamePlayerDTO(GamePlayer gamePlayer) { this.gamePlayer = gamePlayer; }
+
+    public Map<String, Object> makeGamePlayerDTO() {
+        Map<String,Object> dto = new LinkedHashMap<>();
+
+        PlayerDTO pDTO = new PlayerDTO(gamePlayer.getPlayer());
+
+        dto.put("id",gamePlayer.getid());
+        dto.put("player", pDTO.makePlayerDTO());
+
+        return dto;
+    }
+
+    public Map<String,Object> makeGameViewDTO(){
+
+        GameDTO gDTO = new GameDTO(gamePlayer.getGame());
+
+        Map<String,Object> dto = gDTO.makeGameDTO();
+
+        List<Ship> ships = gamePlayer.getShips().stream().collect(Collectors.toList());
+
+        Set<GamePlayer> gamePlayers = gamePlayer.getGame().getGamePlayers();
+
+        List<Salvo> salvoes = gamePlayers
+                .stream()
+                .flatMap(gp -> gp.getSalvoes().stream())
+                .collect(Collectors.toList());
+
+        dto.put("ships",ships
+                .stream()
+                .map(ship -> {
+                    ShipDTO shipDTO = new ShipDTO(ship);
+                    return shipDTO.makeShipDTO();})
+                .collect(Collectors.toList()));
+
+        dto.put("salvoes",salvoes
+                .stream()
+                .map(salvo -> {
+                    SalvoDTO salvoDTO = new SalvoDTO(salvo);
+                    return salvoDTO.makeSalvoDTO();
+                }).collect(Collectors.toList()));
+
+        return dto;
+    }
+}
---
+++ src/main/java/com/codeoftheweb/salvo/model/Game.java
@@ -0,0 +1,44 @@
+package com.codeoftheweb.salvo.model;
+
+import com.codeoftheweb.salvo.model.GamePlayer;
+import com.codeoftheweb.salvo.model.Player;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static java.util.stream.Collectors.toList;
+
+@Entity
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private LocalDateTime creationDate;
+
+    @OneToMany(mappedBy="game", fetch=FetchType.EAGER)
+    private Set<GamePlayer> gamePlayers = new HashSet<>();
+
+    public Game() { }
+
+    public Game(LocalDateTime  creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public long getid() {
+        return id;
+    }
+    public void setid(long id) { this.id = id; }
+
+    public LocalDateTime  getCreationDate() { return creationDate; }
+    public void setCreationDate(LocalDateTime  creationDate) { this.creationDate = creationDate; }
+
+    public Set<GamePlayer> getGamePlayers() { return gamePlayers; }
+
+    public List<Player> getPlayers() {
+        return gamePlayers.stream().map(player -> player.getPlayer()).collect(toList());
+    }
+}
