9ff97a3cd17825d058998e4e08e837f348fce56b
franpassa <francopassarelli94@gmail.com>
2020-12-10 16:27:21 -0300

    Agrego informacion de entrega 3 de la Parte 2

---
+++ src/main/java/com/codeoftheweb/salvo/controller/ShipController.java
@@ -0,0 +1,74 @@
+package com.codeoftheweb.salvo.controller;
+
+import com.codeoftheweb.salvo.model.GamePlayer;
+import com.codeoftheweb.salvo.model.Player;
+import com.codeoftheweb.salvo.model.Ship;
+import com.codeoftheweb.salvo.repository.GamePlayerRepository;
+import com.codeoftheweb.salvo.repository.PlayerRepository;
+import com.codeoftheweb.salvo.repository.ShipRepository;
+import com.codeoftheweb.salvo.util.Util;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api")
+public class ShipController {
+
+    @Autowired
+    private GamePlayerRepository repoGamePlayers;
+
+    @Autowired
+    private PlayerRepository repoPlayers;
+
+    @Autowired
+    private ShipRepository repoShips;
+
+    @RequestMapping(value = "/games/players/{gamePlayerId}/ships", method= RequestMethod.POST)
+    ResponseEntity<Map<String,Object>> addShips(@PathVariable long gamePlayerId, @RequestBody Set<Ship> ships, Authentication authentication){
+        GamePlayer gamePlayer = repoGamePlayers.findById(gamePlayerId).orElse(null);
+        Player player = repoPlayers.findByEmail(authentication.getName());
+
+        if(Util.isGuest(authentication)){
+            return new ResponseEntity<>(Util.makeMap("error","Not logged in"), HttpStatus.UNAUTHORIZED);
+        }
+
+        if(gamePlayer == null){
+            return new ResponseEntity<>(Util.makeMap("error","There is no player with given ID"), HttpStatus.UNAUTHORIZED);
+        }
+
+        if(!(gamePlayer.getPlayer().getid() == player.getid())){
+            return new ResponseEntity<>(Util.makeMap("error","This is not your board"), HttpStatus.FORBIDDEN);
+        }
+
+        int sizeOldShips = gamePlayer.getShips().size();
+
+        if(sizeOldShips != 0){
+            return new ResponseEntity<>(Util.makeMap("error","User already have ships"), HttpStatus.FORBIDDEN);
+        }
+
+        int sizeNewShips = ships.size();
+        if(sizeNewShips != 5){
+            return new ResponseEntity<>(Util.makeMap("error","You need five ships"), HttpStatus.FORBIDDEN);
+        }
+
+        List<Ship> newShips = gamePlayer.getShips()
+                .stream()
+                .map(ship -> {
+                    ship.setGameplayer(gamePlayer);
+                    return ship;})
+                .collect(Collectors.toList());
+
+        repoShips.saveAll(ships);
+
+        return new ResponseEntity<>(Util.makeMap("OK","Added ships."),HttpStatus.CREATED);
+    }
+}
